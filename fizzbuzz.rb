86183c096bbbebf5f150230c20f38c920fe6df7b
Tiffany Tang <tifftang22@gmail.com>
2019-07-06 05:15:50 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,5 +1,5 @@
 # Don't forget! This file needs to be 'required' in its spec file
-# See README.md for instructions on how to do this
+puts "Enter any number to see if it will Fizz or Buzz!"# See README.md for instructions on how to do this
 int = gets.chomp.to_i
 def fizzbuzz(int)
   if (int % 3 == 0) && (int % 5 != 0)
