cf55cd2e866ad2e5ce75fa417e7adbb5a980a7a3
Tiffany Tang <tifftang22@gmail.com>
2019-07-06 05:17:02 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,6 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-puts "Enter any number to see if it will Fizz or Buzz!"
-int = gets.chomp.to_i
+
 def fizzbuzz(int)
   if (int % 3 == 0) && (int % 5 != 0)
     puts "Fizz"
@@ -13,5 +12,3 @@ def fizzbuzz(int)
     puts nil
   end
 end
-
-fizzbuzz(int)
